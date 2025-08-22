1483f04a991f132b755fd6abdcd1a67f2f4289cd
Teddy <teddytennant@icloud.com>
2025-08-22 11:42:39 -0400

    simplified codebase, and fixes

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,20 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import chatSlice from './slices/chatSlice';
-import authSlice from './slices/authSlice';
-import settingsSlice from './slices/settingsSlice';
 
 export const store = configureStore({
   reducer: {
     chat: chatSlice,
-    auth: authSlice,
-    settings: settingsSlice,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
-      },
-    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
